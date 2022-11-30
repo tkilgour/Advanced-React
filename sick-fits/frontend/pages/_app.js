04a5ebcfd5d5eee079ae562c411ed696ec638245
Thomas Kilgour <thomas@kilgour.co>
2022-11-30 16:20:46 -0500

    add loading bar

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,12 @@
+import nProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
