39e35056522981600c4877d9de64ac831178bd4d
Thomas Kilgour <thomas@kilgour.co>
2022-11-29 21:47:16 -0500

    setting up header

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 export default function Page({ children }) {
   return (
     <div>
+      <Header />
       <h1>I'm a heading</h1>
       {children}
     </div>
